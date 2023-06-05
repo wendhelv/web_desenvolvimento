edac03ef577acfa678f90c27e6f65bdd3da6c592
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2023-06-05 20:08:30 -0300

    first commit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,7 +5,7 @@ export default function Root() {
     <div className="container">
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <span className="fs-4">Felipe Noticias</span>
+          <span className="fs-4">wendhel web</span>
         </a>
       </header>
       <main>
